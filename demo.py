fd7fb4e1ac6c414dd8e780e1d2e071c6de1aef2f
davedrzewicki <dadrzewicki@gmail.com>
2018-07-17 20:33:29 -0400

    added loop through several machine learning algorithms. returns highest accuracy and corresponding algorithms. note that answers change when re run.

--- demo.py
+++ demo.py
@@ -1,11 +1,24 @@
-from sklearn import tree
+import numpy as np
+#import matplotlib.pyplot as plt
 
-clf = tree.DecisionTreeClassifier()
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.naive_bayes import GaussianNB
 
-# CHALLENGE - create 3 more classifiers...
-# 1
-# 2
-# 3
+names = ["Nearest Neighbors", "Linear SVM",
+         "Decision Tree", "Random Forest",
+         "Naive Bayes"]
+
+classifiers = [
+    KNeighborsClassifier(3),
+    SVC(kernel="linear", C=0.025),
+    DecisionTreeClassifier(max_depth=5),
+    RandomForestClassifier(max_depth=5, n_estimators=10, max_features=1),
+    GaussianNB(),
+    ]
+
 
 # [height, weight, shoe_size]
 X = [[181, 80, 44], [177, 70, 43], [160, 60, 38], [154, 54, 37], [166, 65, 40],
@@ -15,29 +28,43 @@ X = [[181, 80, 44], [177, 70, 43], [160, 60, 38], [154, 54, 37], [166, 65, 40],
 Y = ['male', 'male', 'female', 'female', 'male', 'male', 'female', 'female',
      'female', 'male', 'male']
 
-tally = []
 
-xNew = []
-yNew = []
+array = []
 
-#for i in range(0,9):
+for i in range(0, len(names)): # loops through the classifiers that I've chosen
+    clf = classifiers[i]
 
-for i in range(len(X)):
-    test = [X[i]]
-    ans = Y[i]
-    xNew = list(X)
-    yNew = list(Y)
-    del xNew[i]
-    del yNew[i]
-    clf = clf.fit(xNew, yNew)
-    prediction = clf.predict(test)
-    value = int(prediction == ans)
-    tally.append(value)
-    #print(xNew, '\n', yNew, '\n', prediction)
+    tally = []
+    xNew = []
+    yNew = []
+    x = 0
 
-
-print(tally)
-x = sum(tally)/len(X)
-print(sum(tally), 'out of', len(X), 'Accuracy:', '{0:.2f}'.format(x))
+    for j in range(len(X)): # loops through each value of X matrix, calculates accuracy of all "leave one out" predictions
+        test = [X[j]]
+        ans = Y[j]
+        xNew = list(X)
+        yNew = list(Y)
+        del xNew[j]
+        del yNew[j]
+        clf = clf.fit(xNew, yNew)
+        prediction = clf.predict(test)
+        value = int(prediction == ans)
+        tally.append(value) # 1 if the value was correctly predicted
 
 
+    x = sum(tally)/len(X) # this gives an accuracy
+    array.append([names[i], '{0:.2f}'.format(x)])
+
+print(array) # prints method and corresponding accuracy. Closer to 1 the better
+
+mx = max([float(row[1]) for row in array])
+
+value = []
+
+for i in range(0, len(array)):
+    row = array[i]
+    if float(row[1]) == mx:
+        value.append(row[0])
+
+
+print('best accuracy is', '{0:.2f}'.format(mx), 'which is obtained by', value)
